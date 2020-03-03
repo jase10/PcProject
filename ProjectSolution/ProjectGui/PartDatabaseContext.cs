ab41236471fba2acde36ec135572cb5e5bcacb54
Jase <jasee927@gmail.com>
2020-03-03 18:26:07 +0000

    Changed window1 to cpuWindow

---
+++ ProjectSolution/ProjectGui/PartDatabaseContext.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ProjectGui
+{
+    public partial class PartDatabaseContext : DbContext
+    {
+        public PartDatabaseContext()
+        {
+        }
+
+        public PartDatabaseContext(DbContextOptions<PartDatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Cpu> Cpu { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source = localhost; Initial Catalog= PartDatabase; Persist Security Info = True; User ID = SA; Password = Passw0rd2018");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cpu>(entity =>
+            {
+                entity.ToTable("CPU");
+
+                entity.Property(e => e.CpuId).HasColumnName("CPU_ID");
+
+                entity.Property(e => e.CpuName)
+                    .HasColumnName("CPU_Name")
+                    .HasColumnType("text");
+                entity.Property(e => e.CoreCount)
+                .HasColumnName("CoreCount")
+                .HasColumnType("int");
+                entity.Property(e => e.CoreClock)
+                .HasColumnName("CoreClock")
+                .HasColumnType("float");
+                entity.Property(e => e.BoostClock)
+                .HasColumnName("BoostClock")
+                .HasColumnType("float");
+
+                entity.Property(e => e.Tdp).HasColumnName("TDP");
+                entity.Property(e => e.Price)
+                .HasColumnName("Price")
+                .HasColumnType("float");
+
+
+
+
+
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ ProjectSolution/ProjectGui/cpuWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjectGui
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public List<Cpu> CPU { get; set; }
+        public Window1()
+        {
+            InitializeComponent();
+            
+
+            
+
+        }
+
+        private void Home_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow win1 = new MainWindow();
+            win1.Show();
+            
+        }
+    }
+}
